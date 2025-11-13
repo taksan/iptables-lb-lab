66efbb99af39c3319e05037fa8099685142be360
Takeuchi <g.takeuchi@gmail.com>
2025-11-13 08:11:06 -0300

    fix: bound ip

--- tcp-proxy.sh
+++ tcp-proxy.sh
@@ -1,1 +1,1 @@
-docker run -d --name tcp_proxy --network lbnet0 -p 192.168.18.5:27017:27017 alpine/socat TCP4-LISTEN:27017,fork TCP4:11.11.11.20:27017
+docker run -d --name tcp_proxy --network lbnet0 -p 0.0.0.0:27017:27017 alpine/socat TCP4-LISTEN:27017,fork TCP4:11.11.11.20:27017
